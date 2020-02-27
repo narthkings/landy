3d096175ce3a7b73dd0181f5d6a50386d07439d5
Olasunkanmi Ismail <olasunkanmiismail2@gmail.com>
2020-02-27 12:29:06 +0100

    Corrected some bugs

--- components/landingPage.vue
+++ components/landingPage.vue
@@ -1,91 +1,79 @@
 <template>
-  <section class>
-    <v-row ma-auto wrap>
-      <v-col cols="12" lg="6">
-        <div class="resizing display-4 font-weight-medium">Learn Japanese</div>
-        <div
-          class="subtitle-2 text-down"
-          style="max-width:25rem "
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, nesciunt molestiae ex inventore quibusdam id architecto quos cupiditate nobis magnam eum voluptatem quas quis obcaecati dolor vero veritatis similique alias.</div>
-        <v-btn style="margin-top:3rem" color="success">Get started</v-btn>
+<section class>
+  <v-row wrap>
+    <v-col cols="12" lg="6">
+      <div class="resizing display-4 font-weight-medium">Learn Japanese</div>
+      <div class="subtitle-2 text-down" style="max-width:25rem ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, nesciunt molestiae ex inventore quibusdam id architecto quos cupiditate nobis magnam eum voluptatem quas quis obcaecati dolor vero veritatis similique alias.</div>
+
+      <v-btn depressed width="180" height="45" class="mt-12 body-2 font-weight-bold" color="success">Get started</v-btn>
+    </v-col>
+
+    <v-col class="text-center" cols="12" lg="6">
+      <img src="/img/undraw_book_lover_mkck.png" class="hero__image">
+    </v-col>
+  </v-row>
+
+  <v-container>
+    <v-row wrap class="mt-5">
+      <v-col cols="12" md="6" lg="4">
+        <div class="d-flex justify-center">
+          <v-avatar color="indigo ">
+            <v-icon dark>mdi-book-open-page-variant</v-icon>
+          </v-avatar>
+        </div>
+
+        <div class="mt-4 body-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
       </v-col>
-      <v-col cols="12" lg="6">
-        <v-img
-          src="/img/undraw_book_lover_mkck.png"
-          lazy-src="/img/undraw_book_lover_mkck.png"
-          aspect-ratio="1"
-          class="justify-center"
-          max-width="500"
-          max-height="400"
-          contain
-        ></v-img>
+
+      <v-col cols="12" md="6" lg="4">
+        <div class="d-flex justify-center">
+          <v-avatar color="indigo ">
+            <v-icon dark>mdi-youtube-subscription</v-icon>
+          </v-avatar>
+        </div>
+
+        <div class="mt-4 body-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
+      </v-col>
+
+      <v-col cols="12" md="6" lg="4">
+        <div class="d-flex justify-center align-content-center">
+          <v-avatar color="indigo ">
+            <v-icon dark>mdi-headphones</v-icon>
+          </v-avatar>
+        </div>
+
+        <div class="mt-4 body-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
       </v-col>
     </v-row>
-    <v-container>
-      <v-row wrap class="mt-5">
-        <v-col cols="12" md="6" lg="4">
-          <div class="d-flex justify-center">
-            <v-avatar color="indigo ">
-              <v-icon dark>mdi-book-open-page-variant</v-icon>
-            </v-avatar>
+  </v-container>
+
+  <v-container class="white--text mt-5">
+    <v-row>
+      <v-col cols="12" lg="6">
+        <div class="title">
+          Reach out to us
+          <div>
+            <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="register">
+              <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+
+              <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+              <v-textarea rows="1" prepend-icon="mdi-comment"></v-textarea>
+
+              <!-- <v-btn type="submit" color="primary" depressed rounded height="45" width="180" class="mt-6">Sign Up</v-btn> -->
+            </v-form>
           </div>
-
-          <div
-            class="mt-4 body-1"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
-        </v-col>
-
-        <v-col cols="12" md="6" lg="4">
-          <div class="d-flex justify-center">
-            <v-avatar color="indigo ">
-              <v-icon dark>mdi-youtube-subscription</v-icon>
-            </v-avatar>
-          </div>
-
-          <div
-            class="mt-4 body-1"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
-        </v-col>
-
-        <v-col cols="12" md="6" lg="4">
-          <div class="d-flex justify-center align-content-center">
-            <v-avatar color="indigo ">
-              <v-icon dark>mdi-headphones</v-icon>
-            </v-avatar>
-          </div>
-
-          <div
-            class="mt-4 body-1"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi, veritatis amet ut minima molest modi! Fugiat quos molestias eaque, architecto tenetur ratione</div>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container class="white--text mt-5" grid-list-xs>
-      <v-row wrap>
-        <v-col cols="12" lg="6">
-          <div class="title">
-            Reach out to us
-            <div>
-              <v-form ref="form" v-model="valid" lazy-validation>
-                <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
-
-                <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-
-                <v-textarea rows="1" prepend-icon="mdi-comment"></v-textarea>
-              </v-form>
-            </div>
-          </div>
-        </v-col>
-        <v-col cols="12" lg="6">
-          <div class="title">Address</div>
-          <div
-            class="body-1"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laudantium blanditiis adipisci minima possimus facilis quae illum. Commodi eum</div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </section>
+        </div>
+      </v-col>
+      <v-col cols="12" lg="6">
+        <div class="title">Address</div>
+        <div class="body-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laudantium blanditiis adipisci minima possimus facilis quae illum. Commodi eum</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</section>
 </template>
+
 <script>
 export default {
   name: 'landingPage',
@@ -103,13 +91,21 @@ export default {
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
       ]
     }
-  }
+  },
+
+  methods: {
+    register() {
+      console.log("Just clicked")
+    }
+  },
 }
 </script>
-<style >
+
+<style lang="scss" scoped>
 .text-down {
   margin-top: 1.5rem;
 }
+
 @media screen and (max-width: 320px) {
   .text-down {
     margin-top: 0.1rem;
@@ -121,6 +117,7 @@ export default {
     margin-top: 0.3rem;
   }
 }
+
 @media screen and (max-width: 960px) {
   .text-down {
     max-width: 42rem !important;
@@ -134,12 +131,24 @@ export default {
     font-weight: 600 !important;
   }
 }
+
 @media screen and (max-width: 320px) {
   .v-application .resizing {
     font-size: 2.4rem !important;
   }
 }
+
 .text {
   text-decoration: none;
+}
+
+.hero__image {
+  max-width: 500px;
+  max-height: 400px;
+
+  @media screen and (max-width: 600px) {
+    max-width: 300px;
+  }
+
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,53 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-      v-if="this.$vuetify.breakpoint.xsOnly"
-      color="deep-purple lighten-5"
-    >
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar class="deep-purple lighten-5" :clipped-left="clipped" fixed app flat>
-      <v-row v-if="this.$vuetify.breakpoint.xsOnly">
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      </v-row>
-      <v-spacer v-if="this.$vuetify.breakpoint.xsOnly" />
-      <v-toolbar-title class="ml-4">
-        <nuxt-link class="black--text font-weight-bold text-style ml-4" to="/">Bilinguage</nuxt-link>
-      </v-toolbar-title>
-      <v-spacer v-if="this.$vuetify.breakpoint.smAndUp" />
-      <v-toolbar-title
-        class="font-weight-medium move-right"
-        v-if="this.$vuetify.breakpoint.smAndUp"
-      >
-        <nuxt-link class="black--text text-style ml-5" to="/">
-          <span>
-            Courses
-            <v-icon right>mdi-chevron-down</v-icon>
-          </span>
-        </nuxt-link>
-        <nuxt-link class="black--text text-style ml-5" to="/why_us">Why us</nuxt-link>
-        <nuxt-link class="black--text text-style ml-5" to="/">Support</nuxt-link>
+<v-app>
+  <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app v-if="this.$vuetify.breakpoint.xsOnly" color="deep-purple lighten-5">
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" nuxt exact>
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 
-        <v-btn to="/" class="ma-2 round font-weight-bold" rounded outlined color="black">Get Started</v-btn>
-      </v-toolbar-title>
-    </v-app-bar>
-    <v-content class="my-bg white--text">
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
+  <v-app-bar color="deep-purple lighten-5" dark :clipped-left="clipped" fixed app flat>
+    <v-app-bar-nav-icon color="black" v-if="this.$vuetify.breakpoint.xsOnly" @click.stop="drawer = !drawer" />
 
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+    <v-spacer v-if="this.$vuetify.breakpoint.xsOnly" />
+
+    <v-toolbar-title class="ml-4">
+      <nuxt-link class="black--text font-weight-bold text-style ml-4" to="/">Bilinguage</nuxt-link>
+    </v-toolbar-title>
+
+    <v-spacer v-if="this.$vuetify.breakpoint.smAndUp" />
+    
+    <v-toolbar-title class="font-weight-medium move-right" v-if="this.$vuetify.breakpoint.smAndUp">
+      <nuxt-link class="black--text text-style ml-5" to="/">
+        <span>
+          Courses
+          <v-icon right>mdi-chevron-down</v-icon>
+        </span>
+      </nuxt-link>
+      <nuxt-link class="black--text text-style ml-5" to="/why_us">Why us</nuxt-link>
+      <nuxt-link class="black--text text-style ml-5" to="/">Support</nuxt-link>
+
+      <v-btn to="#" class="ml-4 font-weight-bold" rounded outlined color="black">Get Started</v-btn>
+    </v-toolbar-title>
+  </v-app-bar>
+
+  <v-content class="my-bg white--text">
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-content>
+
+  <v-footer :fixed="fixed" app>
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+  </v-footer>
+</v-app>
 </template>
 
 <script>
@@ -64,8 +57,7 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
+      items: [{
           icon: 'mdi-apps',
           title: 'Courses',
           to: '/'
@@ -95,18 +87,21 @@ export default {
 }
 </script>
 
-<style >
+<style>
 .text-style {
   cursor: pointer;
   text-decoration: none;
 }
+
 .round {
   /* border-radius: 3rem; */
   padding: 1.5rem;
 }
+
 .move-right {
   margin-right: 4rem;
 }
+
 .my-bg {
   background: #9aa1f0;
   /* background: #878bc72f; */
